8f7c25b8fe4e1fb3eb5d1d6de4755ec9532eaed5
TheWolf534 <ercoleafageme@hotmail.com>
2021-11-01 11:33:45 +0000

    update

--- ballsCombinations.py
+++ ballsCombinations.py
@@ -1,23 +1,30 @@
-Colour = ["Pink","Brown","Black"]
-Hair = ["Bald","Curly hair", "Straight hair","Long hair","Braided hair"]
-Accesories = ["Tattoes", "Piercings"]
+skin_colour = ["Pink skin","Brown skin","Black skin"]
+hair = ["Bald","Curly hair", "Straight hair","Long hair","Braided hair"]
+hair_colour = ["Brown hair","Blond hair"]
+accesories = ["Tattoes", "Piercings"]
 ball_comb = []
 
-def ball_combinator(iC,iH,iA):
+def ball_combinator(iSC,iH,iHC,iA):
     
-    ball_comb.append((Colour[iC],Hair[iH],Accesories[iA]))
+    ball_comb.append((skin_colour[iSC],hair[iH],hair_colour[iHC],accesories[iA]))
     
-    if iC != len(Colour)-1:
-        iC += 1
-        ball_combinator(iC,iH,iA)
-    elif iH != len(Hair)-1:
-        iC = 0
+    if iSC != len(skin_colour)-1:
+        iSC += 1
+        ball_combinator(iSC,iH,iHC,iA)
+    elif iH != len(hair)-1:
+        iSC = 0
         iH += 1
-        ball_combinator(iC,iH,iA)
-    elif iA != len(Accesories)-1:
-        iC = 0
+        ball_combinator(iSC,iH,iHC,iA)
+    elif iHC != len(hair_colour)-1:
+        iSC = 0
         iH = 0
+        iHC += 1
+        ball_combinator(iSC,iH,iHC,iA)
+    elif iA != len(accesories)-1:
+        iSC = 0
+        iH = 0
+        iHC = 0
         iA += 1
-        ball_combinator(iC,iH,iA)
+        ball_combinator(iSC,iH,iHC,iA)
     return ball_comb
-print(list(ball_combinator(0,0,0)))
+print(list(ball_combinator(0,0,0,0)))
